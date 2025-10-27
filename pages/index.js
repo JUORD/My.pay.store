29c6b9c98c11dbdc73a537f5cd40423fcf2413bf
JUORD <aoskaer35@gmail.com>
2025-10-27 09:17:17 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>مرحباً بك في موقعك يا قيس 🔥</h1>
+      <p>تم إنشاء أول صفحة بنجاح ✅</p>
+    </div>
+  );
+    }
